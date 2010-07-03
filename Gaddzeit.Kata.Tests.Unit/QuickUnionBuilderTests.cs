0b71e991795b39699e55fc004dfcc23e867d7081
David Gadd <gaddzeit@yahoo.ca>
2010-07-03 15:26:39 -0700

    First commit of the Quick Union algorithm, built as a series of TDD unit tests in C#. Starting point was the description and diagram of Quick Union algorithm in "Algorithms in Java", chapter 1, by Robert Sedgewick.

---
+++ Gaddzeit.Kata.Tests.Unit/QuickUnionBuilderTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Util;
+using NBehave.Spec.NUnit;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class QuickUnionBuilderTests
+    {
+        [Test]
+        public void ConnectMethod_StartEndIntFirstTimeInput_ArrayChangesStartValueToRootOfTreeValue()
+        {
+            const int startPointValue = 3;
+            const int endPointValue = 4;
+            var intArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var expectedResult = new int[] { 0, 1, 2, 4, 4, 5, 6, 7, 8, 9 };
+
+            var sut = new QuickUnionBuilder(intArray);
+            sut.Connect(startPointValue, endPointValue);
+
+            intArray[startPointValue].ShouldEqual(endPointValue);
+            intArray.ShouldEqual(expectedResult);
+        }
+
+        [Test]
+        public void ConnectMethod_StartEndIntSecondTimeInput_ArrayChangesStartValueToRootOfTreeValue()
+        {
+            const int startPointValue = 4;
+            const int endPointValue = 9;
+            var intArray = new int[] { 0, 1, 2, 4, 4, 5, 6, 7, 8, 9 };
+            var expectedResult = new int[] { 0, 1, 2, 4, 9, 5, 6, 7, 8, 9 };
+
+            var sut = new QuickUnionBuilder(intArray);
+            sut.Connect(startPointValue, endPointValue);
+
+            intArray[startPointValue].ShouldEqual(endPointValue);
+            intArray.ShouldEqual(expectedResult);
+        }
+
+        [Test]
+        public void ConnectMethod_StartEndIntThirdTimeInput_ArrayChangesStartValueToRootOfTreeValue()
+        {
+            const int startPointValue = 8;
+            const int endPointValue = 0;
+            var intArray = new int[] { 0, 1, 2, 4, 9, 5, 6, 7, 8, 9 };
+            var expectedResult = new int[] { 0, 1, 2, 4, 9, 5, 6, 7, 0, 9 };
+
+            var sut = new QuickUnionBuilder(intArray);
+            sut.Connect(startPointValue, endPointValue);
+
+            intArray[startPointValue].ShouldEqual(endPointValue);
+            intArray.ShouldEqual(expectedResult);
+        }
+
+        [Test]
+        public void ConnectMethod_StartEndIntFourthTimeInput_ArrayChangesStartValueToRootOfTreeValue()
+        {
+            const int startPointValue = 2;
+            const int endPointValue = 3;
+            var intArray = new int[] { 0, 1, 2, 4, 9, 5, 6, 7, 0, 9 };
+            var expectedResult = new int[] { 0, 1, 9, 4, 9, 5, 6, 7, 0, 9 };
+
+            var sut = new QuickUnionBuilder(intArray);
+            sut.Connect(startPointValue, endPointValue);
+
+            intArray[startPointValue].ShouldEqual(sut.RootOfTreeFor(endPointValue));
+            intArray.ShouldEqual(expectedResult);
+        }
+
+        [Test]
+        public void ConnectMethod_EntireSequenceOfStartPointInputs_EndPointValuesPointToTraversableRootValues()
+        {
+            var intArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var sut = new QuickUnionBuilder(intArray);
+            sut.Connect(3, 4);
+            intArray.ShouldEqual(new int[] { 0, 1, 2, 4, 4, 5, 6, 7, 8, 9 });
+            sut.Connect(4, 9);
+            intArray.ShouldEqual(new int[] { 0, 1, 2, 4, 9, 5, 6, 7, 8, 9 });
+            sut.Connect(8, 0);
+            intArray.ShouldEqual(new int[] { 0, 1, 2, 4, 9, 5, 6, 7, 0, 9 });
+            sut.Connect(2, 3);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 5, 6, 7, 0, 9 });
+            sut.Connect(5, 6);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 6, 6, 7, 0, 9 });
+            sut.Connect(2, 9);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 6, 6, 7, 0, 9 });
+            sut.Connect(5, 9);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 6, 9, 7, 0, 9 });
+            sut.Connect(7, 3);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 6, 9, 9, 0, 9 });
+            sut.Connect(4, 8);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 6, 9, 9, 0, 0 });
+            sut.Connect(5, 6);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 6, 9, 9, 0, 0 });
+            sut.Connect(0, 2);
+            intArray.ShouldEqual(new int[] { 0, 1, 9, 4, 9, 6, 9, 9, 0, 0 });
+            sut.Connect(6, 1);
+            intArray.ShouldEqual(new int[] { 1, 1, 9, 4, 9, 6, 9, 9, 0, 0 });
+            sut.Connect(5, 8);
+            intArray.ShouldEqual(new int[] { 1, 1, 9, 4, 9, 6, 9, 9, 0, 0 });
+        }
+
+    }
+}
---
+++ Gaddzeit.Kata.Util/QuickUnionBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Gaddzeit.Kata.Util
+{
+    public class QuickUnionBuilder
+    {
+        private readonly int[] _intArray;
+
+        public QuickUnionBuilder(int[] intArray)
+        {
+            _intArray = intArray;
+        }
+
+        public void Connect(int startPointValue, int endPointValue)
+        {
+            //FirstVersionOfAlgorithm(startPointValue, endPointValue);
+            //SecondVersionOfAlgorithm(startPointValue, endPointValue);
+            ThirdVersionOfAlgorithm(startPointValue, endPointValue);
+        }
+
+        private void FirstVersionOfAlgorithm(int startPoint, int endPoint)
+        {
+            _intArray[startPoint] = endPoint;
+        }
+
+        private void SecondVersionOfAlgorithm(int startPoint, int endPoint)
+        {
+            _intArray[startPoint] = RootOfTreeFor(endPoint);
+        }
+
+        private void ThirdVersionOfAlgorithm(int startPoint, int endPoint)
+        {
+            _intArray[RootOfTreeFor(startPoint)] = RootOfTreeFor(endPoint);
+        }
+
+        public int RootOfTreeFor(int index)
+        {
+            var valueFromIndex = _intArray[index];
+            while (valueFromIndex != _intArray[valueFromIndex])
+            {
+                valueFromIndex = _intArray[valueFromIndex];
+            }
+            return valueFromIndex;
+        }
+    }
+}
